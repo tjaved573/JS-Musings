edfad4b377dbb9f5bbca1b67ed600589b3d289f7
Taimoor Javed <taimoorjaved32@gmail.com>
2022-07-24 23:56:23 -0700

    find helper method

---
+++ find_helper.js
@@ -0,0 +1,22 @@
+// use find helper for selecting specific entities from a collection, based on a condition
+
+var users = [
+  { id: 1, admin: false },
+  { id: 2, admin: false },
+  { id: 3, admin: true }
+];
+
+var admin = users.find(function(user){
+   return user.admin === true;
+});
+
+
+var accounts = [
+  { balance: -10 },
+  { balance: 12 },
+  { balance: 0 }
+];
+
+var account = accounts.find(function(account){
+    return account.balance === 12;
+});
