04884c2f45008334e4153fceddf5b0a0bd4460b7
Taimoor Javed <taimoorjaved32@gmail.com>
2022-07-27 00:53:19 -0700

    Add difference between const and let

---
+++ var_declaration.js
@@ -0,0 +1,10 @@
+// 'var' is outdated, so use either of 'let' or 'const'
+
+// const is used when the variable will stay fairly constant through its lifetime
+const nameX = "Taimoor Javed";
+console.log(nameX);
+
+// let is used when the variable may change value in the future.
+let age = 10;
+age += 1;
+console.log(age);
