7ffbd7b8b2c3c2ac9ca232102684ccd46330a72d
Taimoor Javed <taimoorjaved32@gmail.com>
2022-08-02 20:35:18 -0700

    Improve examples for filter helper

--- find_helper.js
+++ find_helper.js
@@ -3,22 +3,18 @@
 var users = [
   { id: 1, admin: false },
   { id: 2, admin: false },
-  { id: 3, admin: true }
+  { id: 3, admin: true },
 ];
 
-var admin = users.find(function(user){
-   return user.admin === true;
+var admin = users.find(function (user) {
+  return user.admin === true;
 });
 
 // returns FIRST value that matches condition. (difference
 // between find() vs filter())
 
-var accounts = [
-  { balance: -10 },
-  { balance: 12 },
-  { balance: 0 }
-];
+var accounts = [{ balance: -10 }, { balance: 12 }, { balance: 0 }];
 
-var account = accounts.find(function(account){
-    return account.balance === 12;
+var account = accounts.find(function (account) {
+  return account.balance === 12;
 });
